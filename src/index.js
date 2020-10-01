96a1b4e955b2b8e7442dce733188a7fbf0469b88
Diwo777 <diego.zegarra79@icloud.com>
2020-10-01 18:10:46 -0400

    finished routes with auth

--- src/index.js
+++ src/index.js
@@ -28,16 +28,27 @@ app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
 
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123'}, 'thisismynewapp', { expiresIn: '7 days'})
-    console.log(token)
-    
-    const data = jwt.verify(token, 'thisismynewapp')
-    console.log(data)
+const pet = {
+    name: 'hal'
 }
-myFunction()
+
+pet.toJSON = function () {
+    console.log(this)
+    return this
+}
+
+console.log(JSON.stringify(pet))
+
+// const jwt = require('jsonwebtoken')
+
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id: 'abc123'}, 'thisismynewapp', { expiresIn: '7 days'})
+//     console.log(token)
+    
+//     const data = jwt.verify(token, 'thisismynewapp')
+//     console.log(data)
+// }
+// myFunction()
 
 // const bcrypt = require('bcryptjs')
 
