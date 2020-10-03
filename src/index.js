22b6ffe67861718fbaa0563a569bbb02383d25ec
Diwo777 <diego.zegarra79@icloud.com>
2020-10-02 22:32:06 -0400

    added sharp

--- src/index.js
+++ src/index.js
@@ -6,15 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-
-const upload = multer({
-    dest: 'images'
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-})
 
 
 //automatically parse incoming JSON to an Object 
@@ -28,7 +19,4 @@ app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
-const router = require('./routers/task')
 
